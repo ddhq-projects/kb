b7a7f465214370e68803d77730e92cede1740298
aiiddqd <1852897+aiiddqd@users.noreply.github.com>
2026-05-05 17:37:00 +0300

    add Yandex.Metrika tracking script to configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,6 +33,25 @@ export default defineConfig({
             href: "/kb/icon.svg",
           },
         },
+        // Yandex.Metrika counter
+        {
+          tag: "script",
+          attrs: { type: "text/javascript" },
+          content: `
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+            ym(52081728, 'init', {webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+          `,
+        },
+        {
+          tag: "noscript",
+          content: '<div><img src="https://mc.yandex.ru/watch/52081728" style="position:absolute; left:-9999px;" alt="" /></div>',
+        },
       ],
       editLink: {
         baseUrl: 'https://github.com/ddhq-projects/kb/blob/main',
